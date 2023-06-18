28e65d892339327530c64291447c676012c4aaa2
MoOsman5 <mohamed1592000gmail.com>
2023-06-18 08:57:43 +0200

    create item list and add items to it

---
+++ src/components/ItemsList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Item from './Item';
+
+const ItemsList = ({itemData}) => {
+  return (
+      <Row >
+        {
+          itemData.length >=1 ?(itemData.map((item)=>{
+            return (
+              <Col key={item.id} sm='4'  className='mb-3 g-5' >
+                <Item item={item}/>
+            
+              </Col>
+            )
+
+          })):(<h3 className='text-center'>No meals</h3>)
+        }
+
+      </Row>
+  );
+}
+
+export default ItemsList;
