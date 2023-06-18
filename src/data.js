a015ce748c762ed19704b4ee98f04469a525bf5d
MoOsman5 <mohamed1592000gmail.com>
2023-06-18 08:59:15 +0200

    create data file and add data to it

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const items=[{
+  id:Math.random(),
+  title:'Pancake',
+  discription:'Pancake with butter and honey ',
+  price:'$5',
+  category:'Breakfast',
+  imgURL:'https://rowadbusiness.com/post/wp-content/uploads/%D8%B5%D9%88%D8%B1-%D8%A7%D9%83%D9%84-%D9%88%D8%B7%D8%B9%D8%A7%D9%85-%D8%A7%D9%84%D9%85%D8%B5%D8%AF%D8%B1-%D8%A8%D8%A7%D9%84%D8%AF%D8%A7%D8%AE%D9%84-1.jpeg'
+},{
+  id:Math.random(),
+  title:'Meat',
+  discription:'Pancake with butter and honey ',
+  price:'$5',
+  category:'lunch',
+  imgURL:'../images/b1.jpeg'
+},{
+  id:Math.random(),
+  title:'Pancake',
+  discription:'Pancake with butter and honey ',
+  price:'$5',
+  category:'dinner',
+  imgURL:'../images/b1.jpeg'
+},{
+  id:Math.random(),
+  title:'Pancake',
+  discription:'Pancake with butter and honey ',
+  price:'$5',
+  category:'Breakfast',
+  imgURL:'../images/b1.jpeg'
+},{
+  id:Math.random(),
+  title:'Pancake',
+  discription:'Pancake with butter and honey ',
+  price:'$5',
+  category:'Breakfast',
+  imgURL:'../images/b1.jpeg'
+},{
+  id:Math.random(),
+  title:'Pancake',
+  discription:'Pancake with butter and honey ',
+  price:'$5',
+  category:'Breakfast',
+  imgURL:'../images/b1.jpeg'
+}]
