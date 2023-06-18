56924e85395a6bbf3bd9658a22dc02e476194d36
MoOsman5 <mohamed1592000gmail.com>
2023-06-18 10:49:07 +0200

    edit data

--- src/data.js
+++ src/data.js
@@ -10,34 +10,34 @@ export const items=[{
   title:'Meat',
   discription:'Pancake with butter and honey ',
   price:'$5',
-  category:'lunch',
-  imgURL:'/images/b1.jpeg'
+  category:'Dinner',
+  imgURL:'/images/m1.jpeg'
 },{
   id:Math.random(),
-  title:'Pancake',
+  title:'pizza',
   discription:'Pancake with butter and honey ',
   price:'$5',
-  category:'dinner',
-  imgURL:'/images/b1.jpeg'
+  category:'Dinner',
+  imgURL:'/images/p1.jpeg'
 },{
   id:Math.random(),
-  title:'Pancake',
+  title:'Burger',
+  discription:'Pancake with butter and honey ',
+  price:'$5',
+  category:'Lunch',
+  imgURL:'/images/bu1.jpeg'
+},{
+  id:Math.random(),
+  title:'Fish',
+  discription:'Pancake with butter and honey ',
+  price:'$5',
+  category:'Lunch',
+  imgURL:'/images/f1.jpeg'
+},{
+  id:Math.random(),
+  title:'Soup',
   discription:'Pancake with butter and honey ',
   price:'$5',
   category:'Breakfast',
-  imgURL:'/images/b1.jpeg'
-},{
-  id:Math.random(),
-  title:'Pancake',
-  discription:'Pancake with butter and honey ',
-  price:'$5',
-  category:'Breakfast',
-  imgURL:'/images/b1.jpeg'
-},{
-  id:Math.random(),
-  title:'Pancake',
-  discription:'Pancake with butter and honey ',
-  price:'$5',
-  category:'Breakfast',
-  imgURL:'/images/b1.jpeg'
+  imgURL:'/images/s1.jpeg'
 }]
